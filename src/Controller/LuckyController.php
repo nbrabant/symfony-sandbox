f1507972731edb2cb46c9f15127c81f8516fdc20
Nicolas Brabant <nbrabant@itm.fr>
2019-01-21 16:11:22 +0100

    Lucky controller complement : add some dependency injection

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 
 class LuckyController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @Route("lucky/number")
      */
     public function number()
     {
+        $this->logger->info('We are loggin!');
+
         $number = random_int(0, 100);
 
         return $this->render('lucky/number.html.twig', compact('number'));
